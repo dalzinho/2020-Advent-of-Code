7ea3a386be6596420937fe0a799803e4e89d7116
jpdal <jpdaly@gmail.com>
2020-12-06 08:45:08 +0000

    day 6 solution

--- src/main/java/day/_6/AllAnsweredYesGroupProcessor.java
+++ src/main/java/day/_6/AllAnsweredYesGroupProcessor.java
@@ -1,22 +1,20 @@
 package day._6;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.function.Function.identity;
 
 public class AllAnsweredYesGroupProcessor implements GroupProcessor {
 
     public long process(List<String> answers) {
-        Map<String, Integer> wordCount = new HashMap<>();
-
-        answers.forEach(answer -> {
-            final String[] split = answer.split("");
-            for (String s : split) {
-                wordCount.merge(s, 1, Integer::sum);
-            }
-        });
-
-        return wordCount.entrySet().stream()
+        return answers.stream()
+                .map(answer -> answer.split(""))
+                .map(Arrays::asList)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toMap(String::valueOf, i -> 1, Integer::sum))
+                .entrySet()
+                .stream()
                 .filter(entry -> entry.getValue() == answers.size())
                 .count();
     }
